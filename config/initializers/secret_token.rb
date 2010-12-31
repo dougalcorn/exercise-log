e94e1584b795aaed4a3434ff46da2700661262ab
Doug Alcorn <dalcorn@carla.local>
2010-12-30 21:17:04 -0500

    rails new exercise-log -O -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExerciseLog::Application.config.secret_token = '930d3e231a3bb749a761a7791cd211fbf1a2fd9d555b70556e81cff90f1dec60f94b94311377421397ba964967a4a48eed36e2c0aeaaec0989dbbad8ef404a66'
